ca339e97ebac62634f0239da6cd92e9475b7ecd5
Rehan959 <rehanshami007@gmail.com>
2021-01-09 16:15:36 -0500

    Quiz app

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-questionList.forEach(question => {
+const section = document.querySelector(".questions");
 
+questionList.forEach((question, index) => {
+  section.innerHTML += ` 
+<div class="question1">
+<p>Q${index + 1} ${question.questionBody}</p>
+<ol>
+    <li>${question.options[0]}</li>
+    <li>${question.options[1]}</li>
+    <li>${question.options[2]}</li>
+</ol>
+</div>
+`;
 });
--- question.js
+++ question.js
@@ -1,17 +1,17 @@
  const questionList = [
     {
-        questionBody : "HTML stands for -",
+        questionBody : "HTML stands for",
         options : ["HighText Machine Language", "HyperText and links Markup Language", "HyperText Markup Language"],
         correctAns : 3,
     },
     {
         questionBody : "Which of the following tag is used for inserting the largest heading in HTML?",
-        options : ["<h3>", "<h1>", "<h4>"],
+        options : ["h3", "h1", "h4"],
         correctAns : 2,
     },
     {
         questionBody : "Which of the following tag is used to insert a line-break in HTML?",
-        options : ["<br>", "<pre>", "<p>"],
+        options : ["br", "pre", "p"],
         correctAns : 1,
     }
 ]
