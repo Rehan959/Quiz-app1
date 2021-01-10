a6cfb439504c189c3816ef259e3f4938efbf1943
Rehan959 <rehanshami007@gmail.com>
2021-01-10 09:09:22 -0500

    Quiz app

--- question.js
+++ question.js
@@ -1,17 +1,23 @@
- const questionList = [
-    {
-        questionBody : "HTML stands for",
-        options : ["HighText Machine Language", "HyperText and links Markup Language", "HyperText Markup Language"],
-        correctAns : 3,
-    },
-    {
-        questionBody : "Which of the following tag is used for inserting the largest heading in HTML?",
-        options : ["h3", "h1", "h4"],
-        correctAns : 2,
-    },
-    {
-        questionBody : "Which of the following tag is used to insert a line-break in HTML?",
-        options : ["br", "pre", "p"],
-        correctAns : 1,
-    }
-]
+const questionList = [
+  {
+    questionBody: "HTML stands for",
+    options: [
+      "HighText Machine Language",
+      "HyperText and links Markup Language",
+      "HyperText Markup Language",
+    ],
+    correctAns: 3,
+  },
+  {
+    questionBody:
+      "Which of the following tag is used for inserting the largest heading in HTML?",
+    options: ["h3", "h1", "h4"],
+    correctAns: 2,
+  },
+  {
+    questionBody:
+      "Which of the following tag is used to insert a line-break in HTML?",
+    options: ["br", "pre", "p"],
+    correctAns: 1,
+  },
+];
