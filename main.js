25ca86d72935c31b10e26c5e47e7f9d75300ccd7
Rehan959 <rehanshami007@gmail.com>
2021-01-16 13:00:55 -0500

    QuizApp

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
+//taking section from html
 const section = document.querySelector(".questions");
+// addding a eventListener
+section.addEventListener("click", clickHandler);
+// storing the answers
+let userAnsList = [];
+// making a function name ClickHandler
+function clickHandler(e) {
+  if (e.target.nodeName.toLowerCase() === "li") {
+    const qNum = +e.path[1].id;
+    userAnsList[qNum] = e.target.innerHTML;
+    console.log(userAnsList);
+    e.target.classList.toggle("selectedAns");
+  }
+}
 
+//  forEach can only be used in case of arrays
 questionList.forEach((question, index) => {
-  section.innerHTML += ` 
-<div class="question1">
-<p>Q${index + 1} ${question.questionBody}</p>
-<ol>
-    <li>${question.options[0]}</li>
-    <li>${question.options[1]}</li>
-    <li>${question.options[2]}</li>
-</ol>
-</div>
-`;
+  if (question.questionBody == "") {
+    return;
+  }
+  section.innerHTML += `
+  <section class="question" id="question${index}">
+  <p>Q${index + 1}. ${question.questionBody}</p>
+  <div id=${index}>
+      <input value="q${index}o0" id="q${index}o1" type="radio" name="${index}" />
+      <label for="q${index}o0">${question.options[0]}</label>
+      <br>
+      <input value="q${index}o1" id="q${index}o1" type="radio" name="${index}" />
+      <label for="q${index}o1">${question.options[1]}</label>
+      <br>
+      <input value="q${index}o2" id="q${index}o1" type="radio" name="${index}" />
+      
+      <label for="q${index}o2">${question.options[2]}</label>
+  </div>
+</section>
+    `;
 });
--- question.js
+++ question.js
@@ -6,18 +6,18 @@ const questionList = [
       "HyperText and links Markup Language",
       "HyperText Markup Language",
     ],
-    correctAns: 3,
+    correctAns: 2,
   },
   {
     questionBody:
       "Which of the following tag is used for inserting the largest heading in HTML?",
     options: ["h3", "h1", "h4"],
-    correctAns: 2,
+    correctAns: 1,
   },
   {
     questionBody:
       "Which of the following tag is used to insert a line-break in HTML?",
     options: ["br", "pre", "p"],
-    correctAns: 1,
+    correctAns: 0,
   },
 ];
