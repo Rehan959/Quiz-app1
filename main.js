2ee72efad5e8e104feac952745d9fd3f085a190b
Rehan959 <rehanshami007@gmail.com>
2021-01-17 12:47:25 -0500

    Quiz App

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 //taking section from html
 const section = document.querySelector(".questions");
+const sumbit = document.querySelector("button");
 // addding a eventListener
-section.addEventListener("click", clickHandler);
+sumbit.addEventListener("click",SubmitHandler);
 // storing the answers
-let userAnsList = [];
+let userAnsList = ["","",""];
 // making a function name ClickHandler
-function clickHandler(e) {
-  if (e.target.nodeName.toLowerCase() === "li") {
-    const qNum = +e.path[1].id;
-    userAnsList[qNum] = e.target.innerHTML;
-    console.log(userAnsList);
-    e.target.classList.toggle("selectedAns");
-  }
-}
-
+ 
 //  forEach can only be used in case of arrays
 questionList.forEach((question, index) => {
   if (question.questionBody == "") {
@@ -23,16 +16,26 @@ questionList.forEach((question, index) => {
   <section class="question" id="question${index}">
   <p>Q${index + 1}. ${question.questionBody}</p>
   <div id=${index}>
-      <input value="q${index}o0" id="q${index}o1" type="radio" name="${index}" />
+      <input value="${
+        question.options[0]
+      }" id="q${index}o1" type="radio" name="${index}" />
       <label for="q${index}o0">${question.options[0]}</label>
       <br>
-      <input value="q${index}o1" id="q${index}o1" type="radio" name="${index}" />
+      <input value="${
+        question.options[1]
+      }" id="q${index}o1" type="radio" name="${index}" />
       <label for="q${index}o1">${question.options[1]}</label>
       <br>
-      <input value="q${index}o2" id="q${index}o1" type="radio" name="${index}" />
+      <input value="${
+        question.options[2]
+      }" id="q${index}o1" type="radio" name="${index}" />
       
       <label for="q${index}o2">${question.options[2]}</label>
   </div>
-</section>
-    `;
+  </section>
+  `;
 });
+
+function SubmitHandler(){
+
+}
